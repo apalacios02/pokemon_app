9617c396e8503339ed286de871e6baaaafca759a
apalacios02 <allan.palacios28@gmail.com>
2024-07-01 12:55:33 -0500

    added search function and up the limit of pokemon to 1000 pokemon.

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,8 +3,14 @@ class PokemonController < ApplicationController
   require 'httparty'
 
   def index
-    response = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=10')
-    @pokemons = response.parsed_response['results']
+    if params[:search]
+      response = HTTParty.get("https://pokeapi.co/api/v2/pokemon?limit=1000")  # Increase limit to get all pokemons
+      pokemons = response.parsed_response['results']
+      @pokemons = pokemons.select { |pokemon| pokemon['name'].start_with?(params[:search]) }
+    else
+      response = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=10')
+      @pokemons = response.parsed_response['results']
+    end
   end
 
   def show
